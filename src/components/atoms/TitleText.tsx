3086f936768705b0306d506c2f6b58dca0aa2138
Miguel <miguelangelagudelolopera@gmail.com>
2025-06-22 16:55:50 -0500

    fix: se aplicó color de texto explícito para mejorar la visibilidad en modo oscuro

--- src/components/atoms/TitleText.tsx
+++ src/components/atoms/TitleText.tsx
@@ -9,6 +9,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginTop: 20,
         fontFamily: 'System',
+        color: '#333',
     },
 });
 
--- src/components/molecules/BreedCard.tsx
+++ src/components/molecules/BreedCard.tsx
@@ -93,6 +93,7 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: '600',
         marginBottom: 8,
+        color: '#333',
     },
     text: {
         fontSize: 14,
